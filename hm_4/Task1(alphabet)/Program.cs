3b10395bb5bc9cd09f06df817971262d1e644e07
xlopec26 <misha.pyshnik@gmail.com>
2019-03-13 12:32:43 +0300

    Solved Task1

--- hm_4/Task1(alphabet)/Program.cs
+++ hm_4/Task1(alphabet)/Program.cs
@@ -6,10 +6,23 @@ using System.Threading.Tasks;
 
 namespace Task1_alphabet_
 {
+
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Вывод английского алфавита в обратном порядке");
+            char letter;
+            int n = 26;
+            for (int i = 122; i >= 97; i--)
+            {
+                letter = System.Convert.ToChar(i);
+                Console.WriteLine("Всего 26 букв. Буквой № " +n + "  является " + letter);
+                //mass[n] = ch;
+                n--; 
+            }
+            Console.ReadLine();
+            //Console.WriteLine("Всего 26 букв. Буквой № " + n  "является " + letter);
         }
     }
 }
