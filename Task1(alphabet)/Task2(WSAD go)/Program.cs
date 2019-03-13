3330cccb848dfefe2a1b2913db05313ca5fb7237
xlopec26 <misha.pyshnik@gmail.com>
2019-03-13 12:08:54 +0300

    Сhange name. Add Sln.

---
+++ Task1(alphabet)/Task2(WSAD go)/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2_WSAD_go_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
